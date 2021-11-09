e6a935912718e0545390769b447e657e16cc01f7
AlexKoloCorp <kolomiets.alex.home@gmail.com>
2021-11-09 20:24:00 +0200

    Добавьте файлы проекта.

---
+++ CharTesting/IAccaunt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEmulator
+{
+    interface IAccauntActivity
+    {
+        void Put();
+        void Withdraw();
+        void Transfer();
+        void TopUpYourPhone();
+    }
+}
